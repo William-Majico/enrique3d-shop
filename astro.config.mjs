6354480d0be39d702d464d98649f14717056d2da
Enrique3D <majicoenrique76@gmail.com>
2025-12-20 20:57:18 -0600

    Modo estatico temporal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+// import vercel from "@astrojs/vercel/serverless";  <-- COMENTA ESTA LÍNEA CON //
+
+export default defineConfig({
+  // output: 'server',       <-- COMENTA ESTA LÍNEA CON //
+  // adapter: vercel(),      <-- COMENTA ESTA LÍNEA CON //
+  
+  integrations: [tailwind()]
+});
+
+// @ts-check
+//import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+//export default defineConfig({});
